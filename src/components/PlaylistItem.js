64399712e855d5b6dca41f69a7c9a6b28561dd83
Michael Richter <mr@osor.de>
2021-03-01 21:12:03 +0100

    rename styled components

--- src/components/PlaylistItem.js
+++ src/components/PlaylistItem.js
@@ -1,19 +1,19 @@
 // import { Avatar } from 'rsuite';
-import * as styles from '../styles/playlist';
+import { Container, Image, Meta, Time, Title, Artist } from '../styles/playlist';
 import Moment from 'react-moment';
 
 function PlaylistItem(props) {
     return (
-        <styles.PlaylistItemContainer>
-            <styles.PlaylistItemImage circle src={props.image} />
-            <styles.PlaylistItemMeta>
-                <styles.PlaylistItemTitle>{props.title}</styles.PlaylistItemTitle>
-                <styles.PlaylistItemArtist>{props.artist}</styles.PlaylistItemArtist>
-            </styles.PlaylistItemMeta>
-            <styles.PlaylistItemTime>
+        <Container>
+            <Image circle src={props.image} />
+            <Meta>
+                <Title>{props.title}</Title>
+                <Artist>{props.artist}</Artist>
+            </Meta>
+            <Time>
                 <Moment format="HH:mm">{props.date}</Moment>
-            </styles.PlaylistItemTime>
-        </styles.PlaylistItemContainer>
+            </Time>
+        </Container>
     );
 }
 
--- src/styles/playlist.js
+++ src/styles/playlist.js
@@ -1,40 +1,40 @@
 import styled from 'styled-components';
 import { Avatar } from 'rsuite';
 
-const PlaylistItemContainer = styled.div`
+const Container = styled.div`
     padding: 16px 8px;
     white-space: nowrap;
     display: flex;
 `;
-const PlaylistItemImage = styled(Avatar)`
+const Image = styled(Avatar)`
     
 `;
-const PlaylistItemMeta = styled.div`
+const Meta = styled.div`
     padding: 0px 16px;
     min-width: 0;
     flex: 2;
 `;
-const PlaylistItemArtist = styled.div`
+const Artist = styled.div`
     overflow: hidden;
     text-overflow: ellipsis;
     min-width: 0;
 `;
-const PlaylistItemTitle = styled.div`
+const Title = styled.div`
     font-weight: bold;
     overflow: hidden;
     text-overflow: ellipsis;
     min-width: 0;
 `;
-const PlaylistItemTime = styled.div`
+const Time = styled.div`
     font-size: 1.3em;
     flex: 1;
 `;
 
 export {
-    PlaylistItemContainer,
-    PlaylistItemImage,
-    PlaylistItemMeta,
-    PlaylistItemArtist,
-    PlaylistItemTitle,
-    PlaylistItemTime,
+    Container,
+    Image,
+    Meta,
+    Artist,
+    Title,
+    Time,
 };
